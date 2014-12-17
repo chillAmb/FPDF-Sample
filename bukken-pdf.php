48a2e2ac08b0acd983f424deed71eab0eef86577
chillAmb <chillout_downtempo@yahoo.co.jp>
2014-12-17 17:57:51 +0900

    first commit

---
+++ bukken-pdf.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin Name: 物件PDF化プラグイン
+    Description: 物件のPDF化を行います
+    Version: 1.014
+    Author: A2sys
+    Author URI: http://a2sy.chillout.jp/
+    License:
+
+*/
+
+require_once(dirname(__FILE__)."/pdf.php");
